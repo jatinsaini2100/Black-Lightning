82b7370531c9c9e1cc77cd2f45cd771a5e567a8e
Aditya <79218915+Paramatin-OP@users.noreply.github.com>
2021-07-31 11:29:22 +0530

    Update lightningrun.py

--- lightningrun.py
+++ lightningrun.py
@@ -1,2 +1,2 @@
 import os
-os.system("git clone https://github.com/Paramatin-OP/Black-Lightning-2 /root/userbot && mkdir /root/userbot/bin/ && cd /root/userbot/ && chmod +x /usr/local/bin/* && python3 -m userbot")
+os.system("git clone https://github.com/keinshin/Black-Lightning /root/userbot && mkdir /root/userbot/bin/ && cd /root/userbot/ && chmod +x /usr/local/bin/* && python3 -m userbot")
